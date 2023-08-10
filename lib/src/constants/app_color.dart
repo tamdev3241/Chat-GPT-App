05856ec5c0ae24dd81dc1394a2b5794aa64cadf5
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-08-10 22:05:59 +0700

    add structure folder

---
+++ lib/src/constants/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color scaffoldBg = Color(0xFF343541);
+  static const Color card = Color(0xFF444654);
+}
---
+++ lib/src/constants/assest_manager.dart
@@ -0,0 +1,6 @@
+class AssetManager {
+  static const String imagePath = "assets/images";
+  static const String chatLogo = "$imagePath/chat_logo.png";
+  static const String openAILogo = "$imagePath/openai_logo.jpg";
+  static const String person = "$imagePath/chat_logo.png";
+}
